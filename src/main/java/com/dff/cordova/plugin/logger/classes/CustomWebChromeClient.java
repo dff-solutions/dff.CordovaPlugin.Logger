ab68cd426931169113504b7f0b096c87f2048b9b
Anthony Nahas <anthony.na@hotmail.de>
2018-01-26 14:15:29 +0100

    Feat: on new console log --> add log object to db

--- src/main/java/com/dff/cordova/plugin/logger/classes/CustomWebChromeClient.java
+++ src/main/java/com/dff/cordova/plugin/logger/classes/CustomWebChromeClient.java
@@ -3,8 +3,13 @@ package com.dff.cordova.plugin.logger.classes;
 import android.content.Context;
 import android.util.Log;
 import android.webkit.ConsoleMessage;
+
 import org.apache.cordova.engine.SystemWebChromeClient;
 import org.apache.cordova.engine.SystemWebViewEngine;
+
+import javax.inject.Inject;
+
+import io.realm.Realm;
 
 /**
  * Created by anahas on 24.02.2017.
@@ -16,19 +21,48 @@ import org.apache.cordova.engine.SystemWebViewEngine;
 public class CustomWebChromeClient extends SystemWebChromeClient {
 
   private static final String TAG = "CustomWebChromeClient";
-  private Context mContext;
+  private Realm mRealm;
 
-  public CustomWebChromeClient(SystemWebViewEngine parentEngine) {
+  @Inject
+  public CustomWebChromeClient(SystemWebViewEngine parentEngine,
+                               Realm mRealm) {
     super(parentEngine);
+    this.mRealm = mRealm;
   }
 
   @Override
   public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
     Log.d(TAG, "onConsoleMessage");
-    if (consoleMessage.message() != null)
-      Log.d(TAG, consoleMessage.sourceId() + " : Line"
-        + consoleMessage.lineNumber() + " : "
+    if (consoleMessage.message() != null) {
+
+
+      Log.d(TAG, "LEVEL: "
+        + consoleMessage.messageLevel()
+        + consoleMessage.sourceId()
+        + " : Line"
+        + consoleMessage.lineNumber()
+        + " : "
         + consoleMessage.message());
+
+      com.dff.cordova.plugin.logger.classes.Log newLog = com.dff.cordova.plugin.logger.classes.Log
+        .newInstance()
+        .build(consoleMessage.messageLevel().name(),
+          consoleMessage.message(),
+          consoleMessage.lineNumber(),
+          consoleMessage.sourceId());
+
+      mRealm.executeTransactionAsync(realm -> realm.insert(newLog),
+        () -> {
+          // Transaction was a success.
+          Log.d(TAG, "Transaction was a success for --> " + newLog);
+        }, error -> {
+          // Transaction failed and was automatically canceled.
+          Log.e(TAG, "Transaction failed and was automatically canceled for --> "
+            + newLog
+            + " Error: "
+            + error);
+        });
+    }
     return true;
   }
 }
