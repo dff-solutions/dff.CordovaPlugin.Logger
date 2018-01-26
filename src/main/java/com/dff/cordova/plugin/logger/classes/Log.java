049c7e585d223b3aa6c2e8e6031782455898b3d2
Anthony Nahas <anthony.na@hotmail.de>
2018-01-26 12:21:56 +0100

    Feat: enhanced the log class in order to be stored in the db

--- src/main/java/com/dff/cordova/plugin/logger/classes/Log.java
+++ src/main/java/com/dff/cordova/plugin/logger/classes/Log.java
@@ -1,10 +1,31 @@
 package com.dff.cordova.plugin.logger.classes;
 
-public class Log {
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
-    private String id;
+import java.util.UUID;
+
+public class Log extends RealmObject {
+
+    @PrimaryKey
+    private String _id;
+
     private String message;
     private int lineNumber;
     private String sourceID;
 
+    public Log() {
+        this._id = UUID.randomUUID().toString();
+    }
+
+    public static Log newInstance() {
+        return new Log();
+    }
+
+    public Log build(String message, int lineNumber, String sourceID) {
+        this.message = message;
+        this.lineNumber = lineNumber;
+        this.sourceID = sourceID;
+        return this;
+    }
 }
