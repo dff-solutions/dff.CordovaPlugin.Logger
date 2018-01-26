53de5f838bad680b4a25842d0ea0a270362e1600
Anthony Nahas <anthony.na@hotmail.de>
2018-01-26 14:11:51 +0100

    Feat: plugin supports now DI

--- src/main/java/com/dff/cordova/plugin/logger/LoggerPlugin.java
+++ src/main/java/com/dff/cordova/plugin/logger/LoggerPlugin.java
@@ -2,12 +2,19 @@ package com.dff.cordova.plugin.logger;
 
 import android.content.Context;
 import android.util.Log;
+import com.dff.cordova.plugin.dagger2.annotations.ApplicationContext;
 import com.dff.cordova.plugin.logger.classes.CustomWebChromeClient;
+import com.dff.cordova.plugin.logger.dagger.components.DaggerLoggerPluginComponent;
+import com.dff.cordova.plugin.logger.dagger.modules.AppModule;
+import com.dff.cordova.plugin.logger.dagger.modules.CordovaModule;
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
-import org.apache.cordova.*;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.engine.SystemWebView;
-import org.apache.cordova.engine.SystemWebViewEngine;
+
+import javax.inject.Inject;
+
 
 /**
  * @author Anthony Nahas
@@ -16,45 +23,33 @@ import org.apache.cordova.engine.SystemWebViewEngine;
  */
 public class LoggerPlugin extends CordovaPlugin {
 
-    private static final String TAG = "LoggerPlugin";
+  private static final String TAG = "LoggerPlugin";
 
-    private Context mContext;
+  @Inject
+  @ApplicationContext
+  Context mContext;
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        Log.d(TAG, "on initialize console plugin");
+  @Inject
+  SystemWebView mSystemWebView;
 
-//    mContext = cordova.getActivity().getApplicationContext();
-//    String packageName = mContext.getPackageName();
-//    Resources resources = mContext.getResources();
-        CordovaActivity ca = (CordovaActivity) cordova.getActivity();
-        SystemWebView view = (SystemWebView) webView.getView();
-        CordovaWebViewEngine webViewEngine = webView.getEngine();
+  @Inject
+  CustomWebChromeClient mCustomWebChromeClient;
 
-        mContext = cordova.getActivity().getApplicationContext();
+  @Inject
+  Realm mRealm;
 
-        // Initialize Realm
-        Realm.init(mContext);
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    Log.d(TAG, "on initialize console plugin");
 
-        // The RealmConfiguration is created using the builder pattern.
-        // The Realm file will be located in Context.getFilesDir() with name "myrealm.realm"
-        RealmConfiguration config = new RealmConfiguration.Builder()
-            .name("logs.realm")
-//            .encryptionKey(getKey())
-            .schemaVersion(0)
-            .deleteRealmIfMigrationNeeded()
-            .build();
+    DaggerLoggerPluginComponent
+      .builder()
+      .appModule(new AppModule(cordova.getActivity().getApplication()))
+      .cordovaModule(new CordovaModule(webView))
+      .build()
+      .inject(this);
 
-        // Get a Realm instance for this thread
-        Realm realm = Realm.getInstance(config);
-
-        if (webViewEngine != null) {
-            Log.d(TAG, "Web view engine is not noll");
-        } else {
-            Log.e(TAG, "Web view engine is noll");
-        }
-//        CustomWebChromeClient c = new CustomWebChromeClient((SystemWebViewEngine) webView.getEngine());
-        view.setWebChromeClient(new CustomWebChromeClient((SystemWebViewEngine) webView.getEngine()));
-    }
+    this.mSystemWebView.setWebChromeClient(mCustomWebChromeClient);
+  }
 }
