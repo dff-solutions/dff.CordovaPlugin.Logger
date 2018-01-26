c759a5900e4dfb2e3afd5c8a11ee6e0b3eb61a6c
Anthony Nahas <anthony.na@hotmail.de>
2018-01-26 12:22:18 +0100

    Feat: integration of realm in the plugin

--- src/main/java/com/dff/cordova/plugin/logger/LoggerPlugin.java
+++ src/main/java/com/dff/cordova/plugin/logger/LoggerPlugin.java
@@ -1,31 +1,58 @@
 package com.dff.cordova.plugin.logger;
 
+import android.content.Context;
 import android.util.Log;
 import com.dff.cordova.plugin.logger.classes.CustomWebChromeClient;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import org.apache.cordova.*;
 import org.apache.cordova.engine.SystemWebView;
 import org.apache.cordova.engine.SystemWebViewEngine;
 
+/**
+ * @author Anthony Nahas
+ * @version 0.1.2
+ * @since 26.01.18
+ */
 public class LoggerPlugin extends CordovaPlugin {
 
     private static final String TAG = "LoggerPlugin";
+
+    private Context mContext;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         Log.d(TAG, "on initialize console plugin");
+
 //    mContext = cordova.getActivity().getApplicationContext();
 //    String packageName = mContext.getPackageName();
 //    Resources resources = mContext.getResources();
         CordovaActivity ca = (CordovaActivity) cordova.getActivity();
         SystemWebView view = (SystemWebView) webView.getView();
-        CordovaWebViewEngine c14124 = webView.getEngine();
+        CordovaWebViewEngine webViewEngine = webView.getEngine();
 
+        mContext = cordova.getActivity().getApplicationContext();
 
-        if (c14124 != null) {
-            Log.d(TAG, "Iam not null");
+        // Initialize Realm
+        Realm.init(mContext);
+
+        // The RealmConfiguration is created using the builder pattern.
+        // The Realm file will be located in Context.getFilesDir() with name "myrealm.realm"
+        RealmConfiguration config = new RealmConfiguration.Builder()
+            .name("logs.realm")
+//            .encryptionKey(getKey())
+            .schemaVersion(0)
+            .deleteRealmIfMigrationNeeded()
+            .build();
+
+        // Get a Realm instance for this thread
+        Realm realm = Realm.getInstance(config);
+
+        if (webViewEngine != null) {
+            Log.d(TAG, "Web view engine is not noll");
         } else {
-            Log.d(TAG, "nullllllll");
+            Log.e(TAG, "Web view engine is noll");
         }
 //        CustomWebChromeClient c = new CustomWebChromeClient((SystemWebViewEngine) webView.getEngine());
         view.setWebChromeClient(new CustomWebChromeClient((SystemWebViewEngine) webView.getEngine()));
--- src/main/java/com/dff/cordova/plugin/logger/classes/Log.java
+++ src/main/java/com/dff/cordova/plugin/logger/classes/Log.java
@@ -1,8 +1,10 @@
 package com.dff.cordova.plugin.logger.classes;
 
+import android.support.annotation.NonNull;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Log extends RealmObject {
@@ -11,13 +13,16 @@ public class Log extends RealmObject {
     private String _id;
 
     private String message;
+    private Date timestamp;
     private int lineNumber;
     private String sourceID;
 
     public Log() {
         this._id = UUID.randomUUID().toString();
+        this.timestamp = new Date();
     }
 
+    @NonNull
     public static Log newInstance() {
         return new Log();
     }
